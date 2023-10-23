a6b56e03cf95bb6a1e2e3db350a986f6d1934940
Maryem Khaoua <maryem.khaoua@gmail.com>
2023-10-23 17:09:16 +0100

    add some modification in panier page

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const btn_moins = document.getElementById("btn-quantity-moins");
 const btn_plus = document.getElementById("btn-quantity-plus");
-var quantity = document.getElementById("btn-quantity")
-
+var quantity = document.getElementById("btn-quantity");
+var currentPrice = document.getElementById("price")
+ 
 let productQnt = 1;
+let totalPrice = currentPrice;
+
 btn_plus.addEventListener("click", function()
 {
     productQnt++;
@@ -16,3 +19,11 @@ btn_moins.addEventListener("click", function()
     quantity.textContent = productQnt;
 
 })
+function validateForm()
+{
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+}
